85add46d3e5a45bf09ea89ebf21755ff924bb149
FrankHenry3487 <zhangchao.frank@foxmail.com>
2018-10-18 23:53:34 +0800

    完成题目，算法不算最好，也还不错

--- leetcode find10/leetcode find10/main.cpp
+++ leetcode find10/leetcode find10/main.cpp
@@ -27,46 +27,45 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<pair<int, int>>> fourSum(vector<pair<int, int>>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<pair<int, int>>> res;
-        int size = (int)nums.size();
-        if (size < 4) return res;
-        for (int i = 0; i < size - 3; i++)
-        {
-            for (int n = size - 1; n > i + 2; n--)
-            {
-                int j = i + 1, k = n - 1;
-                while (j < k)
-                {
-                    if (nums[i].first + nums[n].first + nums[j].first + nums[k].first == 0)
-                        while (j != k)
-                            res.push_back(vector<pair<int, int>>({nums[i], nums[j++], nums[k], nums[n]}));
-                    else if (nums[i].first + nums[j].first + nums[k].first + nums[n].first > 0)
-                        k--;
-                    else
-                        j++;
-                }
-            }
-        }
-        return res;
-    }
-    
     int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
-        vector<pair<int, int>> combine;
-        for (int i = 0; i < A.size(); i++)
-        {
-            combine.emplace_back(A[i], 1);
-            combine.emplace_back(B[i], 2);
-            combine.emplace_back(C[i], 3);
-            combine.emplace_back(D[i], 4);
-        }
-        vector<vector<pair<int, int>>> res = fourSum(combine);
+        int size = (int)A.size();
+        vector<int> AB(size * size, 0);
+        vector<int> CD(size * size, 0);
         int cnt = 0;
-        for(int i = 0; i < res.size(); i++)
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                AB[i + j * size] = A[i] + B[j];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                CD[i + j * size] = C[i] + D[j];
+        
+        sort(AB.begin(), AB.end());
+        sort(CD.begin(), CD.end());
+        
+        int i = 0, j = (int)CD.size() - 1;
+        while (i < CD.size() && ~j)
         {
-            if (res[i][1].second * res[i][2].second * res[i][3].second * res[i][4].second == 24)
-                cnt++;
+            if (AB[i] + CD[j] > 0)
+                j--;
+            else if (AB[i] + CD[j] < 0)
+                i++;
+            else
+            {
+                int up = 1, down = 1;
+                while (i+1 < CD.size() && AB[i] == AB[i+1])
+                {
+                    i++;
+                    up++;
+                }
+                while (~(j-1) && CD[j] == CD[j-1])
+                {
+                    j--;
+                    down++;
+                }
+                cnt += up * down;
+                i++;
+                j--;
+            }
         }
         return cnt;
     }
@@ -74,7 +73,7 @@ public:
     
 int main(int argc, const char * argv[]) {
     Solution s;
-    vector<int> a = {1,2}, b = {-2,-1}, c = {-1,2}, d = {0,2};
-    s.fourSumCount(a, b, c, d);
+    vector<int> a = {-1,-1}, b = {-1,1}, c = {-1,1}, d = {1,-1};
+    cout << s.fourSumCount(a, b, c, d);
     return 0;
 }
