a36da55c8ac5b94ac5ef9e71937f86619aa5e459
FrankHenry3487 <zhangchao.frank@foxmail.com>
2018-10-28 11:54:34 +0800

    3

--- leetcode weekly contest 108/leetcode weekly contest 108/main.cpp
+++ leetcode weekly contest 108/leetcode weekly contest 108/main.cpp
@@ -28,59 +28,29 @@ using namespace std;
 
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& A, int S) {
-        vector<int> aidx;
-        for (int i = 0; i < A.size(); i++)
-            if (A[i] == 1)
-                aidx.push_back(i);
-        if (S > 0)
-        {
-            int f = 0, e = S - 1, res = 0, cnt1 = 0, cnt2 = 0;
-            while (e < aidx.size())
-            {
-                if (f != 0)
-                    cnt1 = aidx[f] - aidx[f - 1];
-                else
-                    cnt1 = aidx[0] + 1;
-                if (e != aidx.size() - 1)
-                    cnt2 = aidx[e + 1] - aidx[e];
-                else
-                    cnt2 = (int)A.size() - aidx[e];
-                res += cnt1 * cnt2;
-                f++;
-                e++;
-            }
-            return res;
-        }
-        else
-        {
-            int res = 0, zn;
-            for (int i = 1; i < aidx.size(); i++)
-            {
-                zn = aidx[i] - aidx[i - 1] - 1;
-                res += (1 + zn) * zn / 2;
-            }
-            if (aidx.size() > 0)
-            {
-                zn = aidx[0];
-                res += (1 + zn) * zn / 2;
-                zn = (int)A.size() - aidx[aidx.size() - 1] - 1;
-                res += (1 + zn) * zn / 2;
-            }
-            else
-            {
-                return ((int)A.size() + 1) * (int)A.size() / 2;
-            }
-            return res;
-        }
+    int minFallingPathSum(vector<vector<int>>& A) {
+        int size = (int)A.size(), res = INT_MAX;
+        vector<vector<int>> out(size, vector<int>(size, INT_MAX));
+        for (int i = 0; i < size; i++)
+            out[0][i] = A[0][i];
+        for (int i = 1; i < size; i++)
+            for (int j = 0; j < size; j++)
+                for (int k = max(j - 1, 0); k < min(size, j + 2); k++)
+                    out[i][j] = min(out[i][j], out[i - 1][k] + A[i][j]);
+        for (auto &v : out[size - 1])
+            res = min(v, res);
+        return res;
     }
 };
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    vector<int> a = {0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0};
+    vector<int> a = {1,2,3};
+    vector<int> b = {4,5,6};
+    vector<int> c = {7,8,9};
+    vector<vector<int>> A = {a,b,c};
     Solution s;
     
-    cout << s.numSubarraysWithSum(a, 3);
+    cout << s.minFallingPathSum(A);
     return 0;
 }
